ce54f6fe45b5553eecf6cfa0a81722f3d69972a9
Risto Vatsar <r.vatsar@gmail.com>
2023-12-09 18:11:23 +0200

    smol change

--- routes/homeTask.js
+++ routes/homeTask.js
@@ -8,7 +8,7 @@ router.use(bodyParser.json());
 router.post("/", (req, res) => {
     if (req.body.name !== "rke" || req.body.code !== "143") {
         res.status(401).json({
-            message: "No access, self destruct initiated...",
+            message: "invalid credentials",
         });
     } else {
         res.status(200).json({ nodeData });
